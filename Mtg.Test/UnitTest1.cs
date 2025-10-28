436aaae7aba21c0610c61893b7d0d43e7dddb02f
Julio Cesar Gualberto Gomes <julio.gualberto@hotmail.com>
2025-10-28 08:00:49 -0300

    ajustando bibliotecas de testes

---
+++ Mtg.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+
+namespace Mtg.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void DeveRetornarHelloWorld()
+        {
+            var helloWorld = "hello world";
+
+            helloWorld.Should().Be("hello world");
+        }
+    }
+}
