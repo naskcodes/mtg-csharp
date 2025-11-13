82eb01ee99ae679dfe935c941edc834f1d2412ff
Julio Cesar Gualberto Gomes <julio.gualberto@hotmail.com>
2025-11-13 15:34:32 -0300

    logar usuario

--- Mtg.Test/Controller/UsuarioTest.cs
+++ Mtg.Test/Controller/UsuarioTest.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using mtg.Api.Controllers;
 using mtg.Api.Controllers.Dto;
+using mtg.Api.Models;
 using mtg.Api.Repositories.interfaces;
 
 namespace Mtg.Test.Controller;
@@ -37,6 +39,42 @@ public class UsuarioTest
         resultado.Should().NotBeNull();
 
         resultado.Value.Should().Be("email ja em uso");
+    }
+    [Fact]
+    public void DeveRetornarErroDeEmailEncontrado()
+    {
+        var email = "error@mail.com";
+
+        mockRepository.Setup(p => p.BuscarUsuarioPorEmail(email)).Returns(null as Usuarios);
+
+        var usuarioMock = new Usuario(mockRepository.Object);
+
+        var erro = () => usuarioMock.UsuarioEntrar(email, "senha");
+
+        erro.Should().Throw<KeyNotFoundException>();    
+    }
+
+    [Fact]
+    public void DeveRetornarOTokenEmailNome()
+    {
+        var usuario = new Usuarios
+        {
+            Nome = "correto",
+            Senha = BCrypt.Net.BCrypt.HashPassword("senha"),
+            Email = "correto@mail.com"
+        };
+
+        mockRepository.Setup(p => p.BuscarUsuarioPorEmail(usuario.Email)).Returns(usuario);
+
+        var controller = new Usuario(mockRepository.Object);
+
+        var resultado = controller.UsuarioEntrar(usuario.Email, "senha");
+
+        var ok = resultado.Should().BeOfType<OkObjectResult>().Which;    
+
+        ok.Should().NotBeNull();
+
+        ok.Value.Should().BeOfType<UsuarioLogadoDto>();
     }
 
     [Fact]
