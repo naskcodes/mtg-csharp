92885d81e8956fa98893941e5b07f770e39e1de5
Julio Cesar Gualberto Gomes <julio.gualberto@hotmail.com>
2025-11-14 10:44:06 -0300

    teste: sucesso e erro ao adicionar carta

--- Mtg.Test/Repositories/UsuarioTests.cs
+++ Mtg.Test/Repositories/UsuarioTests.cs
@@ -6,32 +6,33 @@ using mtg.Api.Repositories;
 
 namespace Mtg.Test.Repositories;
 
-    public class UsuarioTests
+public class UsuarioTests
+{
+    private readonly DbContextOptions<MTGContext> _options;
+    public UsuarioTests()
     {
-        private readonly DbContextOptions<MTGContext> _options;
-        public UsuarioTests() { 
-           _options = new DbContextOptionsBuilder<MTGContext>()
-                .UseInMemoryDatabase(databaseName: "database")
-                .Options;
-        }
-        [Fact]
-        public async Task DeveRetornarOTipoDoIdDeRegistro()
-        {
-            using var context = new MTGContext(_options);
-
-            var repository = new Usuario(context);
-
-            var usuario = new Usuarios
-            {
-                Nome = "Teste",
-                Email = "teste@mail.com",
-                Senha = "senha"
-            };
-
-            var criarUsuario = await repository.CriarUsuario(usuario);
-
-            criarUsuario.Should().NotBe(null);
-
-            criarUsuario.Should().BeOfType(typeof(int));
-        }
+        _options = new DbContextOptionsBuilder<MTGContext>()
+             .UseInMemoryDatabase(databaseName: "database")
+             .Options;
     }
+    [Fact]
+    public async Task DeveRetornarOTipoDoIdDeRegistro()
+    {
+        using var context = new MTGContext(_options);
+
+        var repository = new Usuario(context);
+
+        var usuario = new Usuarios
+        {
+            Nome = "Teste",
+            Email = "teste@mail.com",
+            Senha = "senha"
+        };
+
+        var criarUsuario = await repository.CriarUsuario(usuario);
+
+        criarUsuario.Should().NotBe(null);
+
+        criarUsuario.Should().BeOfType(typeof(int));
+    }
+}
